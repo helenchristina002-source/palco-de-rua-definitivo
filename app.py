869870e57bc7cf470ae9183a477f7f49f2a4269e
helenchristina002-source <helenchristina002@gmail.com>
2025-09-11 16:32:19 -0500

    Create app.py

---
+++ app.py
@@ -0,0 +1,10 @@
+git clone https://github.com/helenchristina002-source/palco-de-rua-definitivo.git
+cd palco-de-rua-definitivo
+
+# DEPENDÊNCIAS
+pip install streamlit pandas
+echo streamlit > requirements.txt
+echo pandas >> requirements.txt
+
+# PASTAS
+mkdir pages data
