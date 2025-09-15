bbc072c9d3b326bd759518f986f9e4aead123373
helenchristina002-source <helenchristina002@gmail.com>
2025-09-14 21:16:30 -0500

    Update app.py

--- app.py
+++ app.py
@@ -6,6 +6,6 @@ st.title("🎭 Palco de Rua – Menu")
 st.write("Escolha um módulo da oficina:")
 
 st.page_link("pages/1_Aquecimento_Reacao.py",
-             label="🔥 Aquecimento – Reação do Espectador", icon="🔥")
+             label="Reação do Espectador", icon="🔥")
 st.page_link("pages/2_Roleta_e_Aplausometro.py",
              label="🎲 Roleta de Cena + 👏 Aplausômetro", icon="🎲")
